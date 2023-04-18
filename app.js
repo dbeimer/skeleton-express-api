91d01e22419cb2673dc1e809e60a7c4dfe0b4470
Beimer Campos <beimercampos.123@gmail.com>
2023-04-18 17:20:36 -0500

    complementing tools added

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const app = express()
 
+app.use(helmet())
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-	res.send('Hello World')
+	res.send('Welcome to my API')
+})
+
+app.post('/user', (req, res) => {
+	res.send({ success: true, message: 'User created successfully' })
 })
 
 app.listen(3000, () => {
